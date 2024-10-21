7650b21db9e72af6b1a05c99626894883d06da1d
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-10-21 13:33:59 +0200

    added "complete task"

--- Apps.FlowFit/Actions/TaskActions.cs
+++ Apps.FlowFit/Actions/TaskActions.cs
@@ -5,6 +5,8 @@ using Apps.FlowFit.Models.Responses.Task;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Utils.Extensions.Http;
+using RestSharp;
 
 namespace Apps.FlowFit.Actions;
 
@@ -19,6 +21,19 @@ public class TaskActions(InvocationContext invocationContext) : FlowFitInvocable
         var request = new FlowFitRequest($"/api/v1/Tasks/{taskIdentifier.TaskId}");
         var task = await Client.ExecuteWithErrorHandling<TaskDto>(request);
         return new(task);
+    }
+
+    [Action("Complete task", Description = "Change the status of a task to completed.")]
+    public async Task CompleteTask([ActionParameter] TaskIdentifier taskIdentifier, [ActionParameter][Display("Note")] string? note)
+    {
+        var request = new FlowFitRequest($"/api/v1/Tasks", Method.Post).WithJsonBody(
+            new
+            {
+                taskId = taskIdentifier.TaskId,
+                note = note ?? string.Empty
+            }
+        );
+        await Client.ExecuteWithErrorHandling(request);
     }
 
     #endregion
